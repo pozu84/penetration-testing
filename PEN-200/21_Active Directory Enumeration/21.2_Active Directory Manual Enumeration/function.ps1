d31013adf0b3232f51dfa6f6d8af9cbb8205a89a
eugeneewe <eugeneewexen@gmail.com>
2024-05-12 14:53:48 +0800

    renamed:    PEN-200/21_Active Directory Enumeration/21.2_Active Directory Manual Enumeration/PEN-200 21.2.3 -> PEN-200/21_Active Directory Enumeration/21.2_Active Directory Manual Enumeration/PEN-200 21.2.3 Q1 	new file:   PEN-200/21_Active Directory Enumeration/21.2_Active Directory Manual Enumeration/PEN-200 21.2.3 Q2 	new file:   PEN-200/21_Active Directory Enumeration/21.2_Active Directory Manual Enumeration/function.ps1

---
+++ PEN-200/21_Active Directory Enumeration/21.2_Active Directory Manual Enumeration/function.ps1
@@ -0,0 +1,15 @@
+function LDAPSearch {
+    param (
+        [string]$LDAPQuery
+    )
+
+    $PDC = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
+    $DistinguishedName = ([adsi]'').distinguishedName
+
+    $DirectoryEntry = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$PDC/$DistinguishedName")
+
+    $DirectorySearcher = New-Object System.DirectoryServices.DirectorySearcher($DirectoryEntry, $LDAPQuery)
+
+    return $DirectorySearcher.FindAll()
+
+}
